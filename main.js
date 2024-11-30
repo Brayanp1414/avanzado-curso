c4c641680788b78454e8b2dcbb8fe7c69f4ed46b
unknown <brayanrojas30253175@gmail.com>
2024-11-30 18:05:59 -0400

    operadores logicos

--- main.js
+++ main.js
@@ -91,5 +91,39 @@ if ( year == 2024 ) {
     alert("Muy bajo");
 } else {
     alert("No es correcto");
-}
+// }
+// operadores logicos
+// or ||
+// alert( true || true ); // true
+// alert( true || false); // true
+// alert( false || true ); // true
+// alert( false || false ); // false
 
+
+let hour = 12;
+let isweekend = false;
+
+if ( hour < 9 || hour > 18 || isweekend){
+     alert("la oficina esta cerrada");
+//   alert("Open!");
+// and &&
+// alert( true && true ); // true
+// alert( true && false ); // false
+// alert(false && true ); // false
+// alert( false && false ); // false
+
+// solution n 
+let userName = prompt('Quien eres tu?', ");
+alert (userName == "Admin" ) {
+    let passWord = prompt('clave?', '');
+    if ( passWord == "TheMaster" ) {
+        alert("Welcome");
+        } else if ( passWord == ''|| password == null)
+        alert( "Cancelado" )
+
+}else{
+    alert("clave incorrepta");
+}else if (userName == ""|| userName == null) {
+     alert("no se quien eres :b")
+     
+}
